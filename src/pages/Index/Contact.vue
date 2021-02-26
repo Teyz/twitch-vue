b613f63081efa1240c58fea3a0e1df2c71c625e5
teyz <contact@bastienrigaud.fr>
2021-02-26 16:48:34 +0100

    contact page wip

--- src/pages/Index/Contact.vue
+++ src/pages/Index/Contact.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="contactRoot">
-    <h2>Schedule an <span>appointment</span></h2>
-    <form method="post">
-      <div class="formRow">
-        <label for="name anime fromTop">Hey my name is </label>
-        <input type="text" id="name" name="name" required />
+    <h2 data-aos="zoom-out-down">Schedule an <span>appointment</span></h2>
+    <form @submit.prevent="sendEmail">
+      <div class="formRow" data-aos="fade-right" data-aos-delay="100">
+        <label for="name">Hey my name is </label>
+        <input type="text" id="name" name="name" required v-model="name" />
         <label for="subject">and I'm looking for</label>
-        <select name="subject anime fromTop" id="subject required">
+        <select name="subject" id="subject required" v-model="project">
           <option value="discuss">Discuss</option>
           <option value="quote">Quotation</option>
           <option value="project">Project</option>
         </select>
       </div>
-      <div class="formRow anime fromTop">
+      <div class="formRow" data-aos="fade-left" data-aos-delay="200">
         <label for="mail">Get in touch with me at</label>
-        <input type="email" id="mail" name="mail" required />
+        <input type="email" id="mail" name="mail" required v-model="email" />
       </div>
-      <div class="formRow anime fromTop">
+      <div class="formRow">
         <input type="checkbox" id="aggrement" name="aggrement" required />
         <label for="aggrement" class="checkbox-label"
           >I accept all terms and conditions</label
         >
       </div>
-      <div class="formRow anime fromTop">
+      <div class="formRow">
         <button type="submit">Send enquiry</button>
       </div>
     </form>
@@ -30,8 +30,46 @@
 </template>
 
 <script>
+import AOS from "aos";
+import "aos/dist/aos.css";
+import emailjs from "emailjs-com";
 export default {
   name: "Contact",
+  data() {
+    return {
+      name: "",
+      project: "",
+      email: "",
+    };
+  },
+  created() {
+    AOS.init();
+  },
+  methods: {
+    sendEmail(e) {
+      console.log(this.name, this.email, this.project);
+      emailjs
+        .sendForm(
+          "service_3myt38i",
+          "template_3ysie0p",
+          e.target,
+          "user_UCQM7Dx1nsQped1PwUprh",
+          {
+            name: this.name,
+            email: this.email,
+            project: this.project,
+          }
+        )
+        .then(
+          (result) => {
+            console.log("SUCCESS!", result.status, result.text);
+          },
+          (error) => {
+            console.log("FAILED...", error);
+          }
+        );
+    },
+  },
 };
 </script>
 
