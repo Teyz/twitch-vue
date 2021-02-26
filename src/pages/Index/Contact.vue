d811537963e7a5e368b519b9d001f35f63fab0f9
teyz <contact@bastienrigaud.fr>
2021-02-26 18:26:42 +0100

    Done: contact form

--- src/pages/Index/Contact.vue
+++ src/pages/Index/Contact.vue
@@ -5,16 +5,16 @@
       <div class="formRow" data-aos="fade-right" data-aos-delay="100">
         <label for="name">Hey my name is </label>
         <input type="text" id="name" name="name" required v-model="name" />
-        <label for="subject">and I'm looking for</label>
-        <select name="subject" id="subject required" v-model="project">
+        <label for="project">and I'm looking for</label>
+        <select name="project" id="project required" v-model="project">
           <option value="discuss">Discuss</option>
           <option value="quote">Quotation</option>
           <option value="project">Project</option>
         </select>
       </div>
       <div class="formRow" data-aos="fade-left" data-aos-delay="200">
-        <label for="mail">Get in touch with me at</label>
-        <input type="email" id="mail" name="mail" required v-model="email" />
+        <label for="email">Get in touch with me at</label>
+        <input type="email" id="email" name="email" required v-model="email" />
       </div>
       <div class="formRow">
         <input type="checkbox" id="aggrement" name="aggrement" required />
@@ -63,12 +63,19 @@ export default {
         .then(
           (result) => {
             console.log("SUCCESS!", result.status, result.text);
+            this.resetForm();
           },
           (error) => {
             console.log("FAILED...", error);
+            this.resetForm();
           }
         );
     },
+  },
+  resetForm() {
+    this.name = "";
+    this.email = "";
+    this.project = "";
   },
 };
 </script>
