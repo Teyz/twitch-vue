d004219f6e372de9d0d0247de2a0690450f0b56c
teyz <contact@bastienrigaud.fr>
2021-05-05 16:09:51 +0200

    Performances 🚀 (Fix) : Google Analytics

--- src/main.js
+++ src/main.js
@@ -4,17 +4,11 @@ import './registerServiceWorker';
 import router from './router';
 import { Wrapper } from "./components";
 import Toast from "vue-toastification";
-import VueGtag from "vue-gtag-next";
 // Import the CSS or use your own!
 import "vue-toastification/dist/index.css";
 
 createApp(App)
   .use(router)
   .use(Toast)
-  .use(VueGtag, {
-    property: {
-      id: "G-4PF7W26DSM"
-    }
-  })
   .component("Wrapper", Wrapper)
   .mount('#app')
